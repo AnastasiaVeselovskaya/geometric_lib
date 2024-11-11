3200b745c677fde41602d42a18ab7d167150a53d
AnastasiaVeselovskaya <veselovskaya.anastasi@gmail.com>
2024-11-11 19:39:01 +0300

    Committing latest version of tests and workflow

--- calculate.py
+++ calculate.py
@@ -2,16 +2,23 @@ import circle
 import square
 
 
-figs = ['circle', 'square']
+figs = ['circle', 'square', 'triangle']
 funcs = ['perimeter', 'area']
 sizes = {}
 
 def calc(fig, func, size):
+	# Выполняет заданную пользователем функцию для определённой фигуры.
+	# Параметры: fig (str) - фигура, для которой требуется провести расчёты; 
+	#			 func (str) - функция, соответствующая необходимым расчётам;
+	#			 size (arr) - параметры фигуры.
+	# Возвращаемое значение: выводит в консоль название, фигуру и значение заданной функции.
+	# Примечание: если задана функция main, calc принимает параметры  с консоли. Вывод не меняется.
+
 	assert fig in figs
 	assert func in funcs
 
 	result = eval(f'{fig}.{func}(*{size})')
-	print(f'{func} of {fig} is {result}')
+	return result
 
 if __name__ == "__main__":
 	func = ''
@@ -25,9 +32,6 @@ if __name__ == "__main__":
 		func = input(f"Enter function name, avaliable are {funcs}:\n")
 	
 	while len(size) != sizes.get(f"{func}-{fig}", 1):
-		size = list(map(int, input("Input figure sizes separated by space, 1 for circle and square\n").split(' ')))
+		size = list(map(int, input("Input figure sizes separated by space, 1 for circle and square, 2-3 for triangle\n").split(' ')))
 	
 	calc(fig, func, size)
-
-
-
